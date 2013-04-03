5be000d3a7d2a0f0da6391e2ea6376f6322ea450
John Hume <duelin.markers@gmail.com>
2013-04-02 22:52:42 -0500

    Add example of multiple extension. Reorganize.

--- test/duelinmarkers/maker_examples.clj
+++ test/duelinmarkers/maker_examples.clj
@@ -1,15 +1,12 @@
 (ns duelinmarkers.maker-examples
   (:use clojure.test duelinmarkers.maker))
 
-(add-prototype :simple {:name "Name"})
-
-(add-prototype :extended [:simple] {:grade 3})
-
-(add-prototype :deeply-extended [:extended] {:dob "1978-02-11"})
-
 (deftest make-from-prototypes
 
   (testing "the simplest prototype"
+
+    (add-prototype :simple {:name "Name"})
+
     (is (= {:name "Name"} (make :simple)))
     (is (= {:name "Override"}
            (make :simple {:name "Override"})
@@ -19,10 +16,30 @@
            (make :simple :other "Val"))))
 
   (testing "a prototype based on another"
+
+    (add-prototype :extended [:simple] {:grade 3})
+
     (is (= {:name "Name" :grade 3} (make :extended)))
     (is (= {:name "Override" :grade 4}
            (make :extended {:name "Override" :grade 4})
            (make :extended :name "Override" :grade 4)))
     (is (= {:name "Name" :grade 3 :other "Val"}
            (make :extended {:other "Val"})
-           (make :extended :other "Val")))))
+           (make :extended :other "Val"))))
+
+  (testing "a deeply extended prototype"
+
+    (add-prototype :very-extended [:extended] {:dob "1978-02-11"})
+
+    (is (= {:name "Name" :grade 3 :dob "1978-02-11"} (make :very-extended))))
+
+  (testing "a multiply-extended prototype"
+
+    (add-prototype :graded-and-ranged {:grade "from graded" :range "all"})
+    (add-prototype :multiply-extended-ab [:extended :graded-and-ranged] {})
+    (add-prototype :multiply-extended-ba [:graded-and-ranged :extended] {})
+
+    (is (= {:name "Name" :grade "from graded" :range "all"}
+           (make :multiply-extended-ab)))
+    (is (= {:name "Name" :grade 3 :range "all"}
+           (make :multiply-extended-ba)))))
