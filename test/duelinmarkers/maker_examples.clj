17bdfc2c003039b50bd95926ad600828e8876ee4
John Hume <duelin.markers@gmail.com>
2013-04-27 18:09:25 -0500

    WIP Make the example into documentation

--- test/duelinmarkers/maker_examples.clj
+++ test/duelinmarkers/maker_examples.clj
@@ -1,70 +1,104 @@
 (ns duelinmarkers.maker-examples
+  "This namespace aims to demonstrate all the features of maker. The
+  `testing` forms below have explanatory commentary littered throughout.
+
+  Some examples use fns from `clojure.data.generators`, which are handy
+  for generating test data, but maker has no dependency on that lib."
   (:use clojure.test)
-  (:require [duelinmarkers.maker :as m :refer (add-prototype
-                                               make
-                                               gen
-                                               gen-from
-                                               gen-from-fn)]
+  (:require [duelinmarkers.maker
+             :refer (add-prototype
+                     make
+                     gen
+                     gen-from
+                     gen-from-fn)]
             [clojure.data.generators :as gen]))
 
 (deftest maker-usage
 
   (testing "the simplest prototype"
 
+    ;; You add prototypes with `add-prototype`, probably in some run-once,
+    ;; test-setup code (like a `clojure.test` `:once` fixture). It takes a
+    ;; key and a map.
+
     (add-prototype :simple {:name "Name"})
 
+    ;; In your tests, call make. When you pass just the key, it returns
+    ;; the corresponding map.
+
     (is (= {:name "Name"} (make :simple)))
+
+    ;; You can override values or add additional kv pairs by additionally
+    ;; passing make either a map or interleaved kvs.
+
     (is (= {:name "Override"}
            (make :simple {:name "Override"})
            (make :simple :name "Override")))
-    (is (= {:name "Name" :other "Val"}
-           (make :simple {:other "Val"})
-           (make :simple :other "Val"))))
+    (is (= {:name "Name" :other "Val" :third 3}
+           (make :simple {:other "Val" :third 3})
+           (make :simple :other "Val" :third 3))))
 
-  (testing "a prototype based on another"
+  (testing "prototypes based on other prototypes"
+
+    ;; When you `add-prototype`, you can extend another prototype, and the
+    ;; map you pass will be conj'd onto the extended prototype map.
 
     (add-prototype :extended [:simple] {:grade 3})
 
     (is (= {:name "Name" :grade 3} (make :extended)))
-    (is (= {:name "Override" :grade 4}
-           (make :extended {:name "Override" :grade 4})
-           (make :extended :name "Override" :grade 4)))
-    (is (= {:name "Name" :grade 3 :other "Val"}
-           (make :extended {:other "Val"})
-           (make :extended :other "Val"))))
-
-  (testing "a deeply extended prototype"
 
     (add-prototype :very-extended [:extended] {:dob "1978-02-11"})
 
     (is (= {:name "Name" :grade 3 :dob "1978-02-11"}
-           (make :very-extended))))
+           (make :very-extended)))
+
+    ;; The base prototype must be defined before you can extend it.
+
+    (is (thrown-with-msg? IllegalArgumentException
+          #"Unknown base prototype\(s\): \(:doesnt-exist\)"
+          (add-prototype :wont-work [:doesnt-exist] {:a 1}))))
 
   (testing "a multiply-extended prototype"
 
+    ;; You can provide multiple base prototypes, and they'll be applied
+    ;; from left to right, so the values in the right-most will "win."
+
     (add-prototype :graded-and-ranged {:grade "from graded" :range "all"})
-    (add-prototype :multiply-extended-ab [:extended :graded-and-ranged] {})
-    (add-prototype :multiply-extended-ba [:graded-and-ranged :extended] {})
+    (add-prototype :multiple-ext-ab [:extended :graded-and-ranged] {})
+    (add-prototype :multiple-ext-ba [:graded-and-ranged :extended] {})
 
     (is (= {:name "Name" :grade "from graded" :range "all"}
-           (make :multiply-extended-ab)))
+           (make :multiple-ext-ab)))
     (is (= {:name "Name" :grade 3 :range "all"}
-           (make :multiply-extended-ba))))
+           (make :multiple-ext-ba))))
 
   (testing "prototypes with independent generated values"
+
+    ;; Use `gen` to provide a fn and args to have values generated when
+    ;; you call make.
 
     (add-prototype :generated {:some-long (gen gen/long)})
     (add-prototype :generated-with-args
                    {:constrained-long (gen gen/long 5 11)})
+
+    (is (number? (:some-long (make :generated))))
+    (is (not= (:some-long (make :generated))
+              (:some-long (make :generated))))
+    ;; Note: That could fail if the `java.util.Random` in
+    ;;       `clojure.data.generators` were feeling lazy.
+
+    (is (<= 5
+            (:constrained-long (make :generated-with-args))
+            10))
+
+    ;; If the fn you pass to gen is `make`, then you get maker-generated
+    ;; maps as values in other maker-generated maps.
+
     (add-prototype :nested-generated
                    {:name "Nested"
                     :gen-simple (gen make :simple)
                     :gen-child (gen make :generated)})
 
-    (is (number? (:some-long (make :generated))))
-    (is (not= (:some-long (make :generated))
-              (:some-long (make :generated))))
-    (is (<= 5 (:constrained-long (make :generated-with-args)) 10))
     (let [nested1 (make :nested-generated)
           nested2 (make :nested-generated)]
       (is (number? (-> nested1 :gen-child :some-long)))
@@ -74,14 +108,26 @@
 
   (testing "prototypes with dependent generated values"
 
+    ;; Generated values can also depend on other values, generated or not,
+    ;; using `gen-from` and `gen-from-fn`.
+
+    ;; `gen-from-fn` will apply the fn passed as the last arg to the
+    ;; values whose keys you pass as the first arg.
+
+    ;; XXX Maybe it would make more sense if it went (gen-from-fn inc :b)?
     (add-prototype :a-depends-b {:a (gen-from-fn [:b] inc)
                                  :b 1})
 
     (is (= {:a 2 :b 1} (make :a-depends-b)))
     (is (= {:a 3 :b 2} (make :a-depends-b {:b 2})))
 
+    ;; gen-from is syntactic sugar for gen-from-fn when you want to create
+    ;; the fn inline with references to the values you depend on.
+
     (add-prototype :phone
-                   {:npa "212" :nxx "555" :xxxx "1212"
+                   {:npa "212"
+                    :nxx "555"
+                    :xxxx "1212"
                     :formatted
                     (gen-from [npa nxx xxxx]
                               (str "(" npa ") " nxx "-" xxxx))})
@@ -91,6 +137,12 @@
            (:formatted
             (make :phone
                   :formatted (gen-from [nxx xxxx] (str nxx "-" xxxx))))))
+
+    ;; So that gen-from is the same as:
+
+    #_(gen-from-fn [:npa :nxx :xxxx]
+                   (fn [npa nxx xxxx]
+                     (str "(" npa ") " nxx "-" xxxx)))
 
     (add-prototype :chained-dependencies {:a (gen-from [b] (inc b))
                                           :b (gen-from-fn [:c :d] +)
