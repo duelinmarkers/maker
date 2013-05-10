64d05c828b79aad73e92e1afa891225e5db0e0ff
John Hume <duelin.markers@gmail.com>
2013-05-10 13:47:59 -0500

    Add profiles 1.3 and 1.4. 1.3 no worky.

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/data.generators "0.1.2"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/data.generators "0.1.2"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
